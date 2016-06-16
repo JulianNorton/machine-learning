28cf02b02b1c6fc08ad5ccda87a9ab548d1ab715
Julian Norton <Julian@JulianNorton.com>
2016-06-15 22:09:48 -0400

    trying to figure out gradient descent

--- machine-learning-ex1/ex1-test.py
+++ machine-learning-ex1/ex1-test.py
@@ -6,11 +6,6 @@ from pylab import scatter, show, title, xlabel, ylabel, plot, contour
 
 a = np.identity(5)
 
-# print(a)
-
-# data=np.loadtxt('ex1data1.txt',delimiter=',')
-
-# print(data)
 
 data=np.loadtxt('ex1data1.txt',delimiter=',')
 plt.scatter(data[:,0],data[:,1])
@@ -31,20 +26,39 @@ x = reshaping_x
 # Gradient Descent
 theta = zeros(shape=(2, 1))
 alpha = 0.01
-iterations = 1500
+iterations = 15
 
 def cost_function(theta, x, y):
-    prediction = dot(x, theta)
-    J = (1.0 / (2*m)) * dot(transpose(prediction - y) , (prediction - y))
+    prediction_y = dot(x, theta)
+    J = (1.0 / (2*m)) * dot(transpose(prediction_y - y) , (prediction_y - y))
+    # print(J, 'cost')
     return J
 
+def gradient_descent(theta, x, y, alpha, iterations):
+    for i in range(iterations):
+        prediction_y = dot(x, theta)
+        print (prediction_y)
+        theta = theta - alpha * (1.0/m) * dot(transpose(prediction_y - y), x)
+    return theta
+    # print('theta =', theta)
+        # print(cost_function(theta, x, y))
 
-print(cost_function(theta, x, y))
+cost_function(theta, x, y)
+theta = gradient_descent(theta, x, y, alpha, iterations)
 
+# visualise outcome
+def plot_solution(x,y,solution):
+    # create figure and axes
+    fig = plt.figure()
+    # split the page into a 1x1 array of subplots and put solution in the first one (111)
+    ax = fig.add_subplot(111)
 
-# z = ones(shape=(m, 2))
-# x=[]
-# X[:, 1] = z
-# X[:, 1] = x
-# X = ones(shape=(m, 2))
-# X[:, 1] = x
+    # plots scatter for x, y
+    ax.scatter(x, y, color='red', marker='o', s=100)
+    # plots solution
+    ax.plot(x, solution, color='green')
+    plt.show()
+
+plot_solution(x,y,dot(x,theta))
+
+# plot_solution([1,2,3], [1,2,3], [1,2,3])
