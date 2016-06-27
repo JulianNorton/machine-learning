79ec783f89aa1b711a1febbcba597aed440008f2
jack-fallon-underwood <jack.underwood@gmail.com>
2016-06-26 20:46:42 -0400

    imported numpy, matplotlib, ect

---
+++ machine-learning-ex3/ex3/neural_networks.py
@@ -0,0 +1,6 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from numpy import zeros, ones, reshape, array, linspace, logspace, add, dot, transpose, shape, negative
+from pylab import scatter, show, title, xlabel, ylabel, plot, contour
+import math
+
