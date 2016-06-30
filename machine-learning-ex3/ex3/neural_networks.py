098814e0ec40fea92746e0af246665fcbddb841d
Julian Norton <Julian@JulianNorton.com>
2016-06-30 10:02:36 -0400

    getting sigmoid function working

--- machine-learning-ex3/ex3/neural_networks.py
+++ machine-learning-ex3/ex3/neural_networks.py
@@ -17,14 +17,34 @@ y = np.array(mat['y'])
 X_row_length = len(X[:, 0])
 X_column_length = len(X[0, :])
 
+# x = m x n
+# theta = n x 1
+# y = m x 1
 m = len(y)
+
 
 
 # x is X but with a column of 1's in the first column
 # becoming a 5000 by 401
 x = np.column_stack((ones(shape(X)[0]), X))
+x_column_length = len(x[0, :])
+n = x_column_length
 
 print(m)
 
+theta = zeros(shape=(n,1))
+
+z = dot(x, theta)
+
+print(z)
+
+def sigmoid_function(z):
+    hypothesis = 1.0 / (1.0 + (math.e)**(-z))
+    # print(hypothesis)
+    return hypothesis
+
+print(sigmoid_function(z))
 # theta gets +1 because that'll be our bias weight ???
 # initial_theta = zeros(shape=(m+1,1))
+
+print(y)
