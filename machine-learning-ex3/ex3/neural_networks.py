18bdcfc940a6ee6d4de93dd1b05fad1363544523
Julian Norton <Julian@JulianNorton.com>
2016-07-02 15:36:10 -0400

    calculating partial derivatives working

--- machine-learning-ex3/ex3/neural_networks.py
+++ machine-learning-ex3/ex3/neural_networks.py
@@ -20,23 +20,19 @@ X_column_length = len(X[0, :])
 # x = m x n
 # theta = n x 1
 # y = m x 1
+# 401 j's
+# 5000 i's
 m = len(y)
-
-
 
 # x is X but with a column of 1's in the first column
 # becoming a 5000 by 401
 x = np.column_stack((ones(shape(X)[0]), X))
 x_column_length = len(x[0, :])
 n = x_column_length
-
-print(m)
-
 theta = zeros(shape=(n,1))
-
 z = dot(x, theta)
+regularization_lambda = 1/m
 
-print(z)
 
 def sigmoid_function(z):
     hypothesis = 1.0 / (1.0 + (math.e)**(-z))
@@ -44,7 +40,39 @@ def sigmoid_function(z):
     return hypothesis
 
 print(sigmoid_function(z))
-# theta gets +1 because that'll be our bias weight ???
-# initial_theta = zeros(shape=(m+1,1))
 
-print(y)
+#         i   j
+# print(x[0, 0])
+
+def cost_function(x,y,i,j):
+    return (sigmoid_function(x[i]) - y[i]) * x[i, j]
+
+def calc_partial_derivatives():
+    calc_partial_derivatives = list()
+    for j in range(n):
+        if j == 0:
+            partial_derivatives = list()
+            for i in range(m):
+                cost = cost_function(x,y,i,j)
+                partial_derivatives.append(cost)
+            partial_derivatives = (1/m) * np.sum(partial_derivatives)
+            calc_partial_derivatives = np.asarray(partial_derivatives)
+        else:
+            partial_derivatives = list()
+            for i in range(m):
+                cost = cost_function(x,y,i,j) + (regularization_lambda * theta[j])
+                partial_derivatives.append(cost)
+            partial_derivatives = (1/m) * np.sum(partial_derivatives)
+            calc_partial_derivatives = np.append(calc_partial_derivatives, partial_derivatives)
+
+calc_partial_derivatives()
+
+
+
+
+
+
+
+
+
+
