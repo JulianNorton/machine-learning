a422b9af4e3a70b54d0a802a9ac81d27e9ad5232
jack-fallon-underwood <jack.underwood@gmail.com>
2016-07-09 16:37:26 -0400

    added some silly loop things

--- machine-learning-ex3/ex3/neural_networks.py
+++ machine-learning-ex3/ex3/neural_networks.py
@@ -5,7 +5,7 @@ from pylab import scatter, show, title, xlabel, ylabel, plot, contour
 import math
 import scipy.io as sio
 
-print('\n', 'Begin script')
+# print('\n', 'Begin script')
 
 mat = sio.loadmat('ex3data1.mat')
 
@@ -30,21 +30,32 @@ x = np.column_stack((ones(shape(X)[0]), X))
 x_column_length = len(x[0, :])
 n = x_column_length
 
-print(m)
+# print(m)
 
 theta = zeros(shape=(n,1))
 
 z = dot(x, theta)
 
-print(z)
+# print(z)
 
 def sigmoid_function(z):
     hypothesis = 1.0 / (1.0 + (math.e)**(-z))
-    # print(hypothesis)
+    #print(hypothesis)
     return hypothesis
 
-print(sigmoid_function(z))
+# print(sigmoid_function(z))
 # theta gets +1 because that'll be our bias weight ???
 # initial_theta = zeros(shape=(m+1,1))
 
-print(y)
+
+exlist = y
+one_v_all = zeros(shape=(n,1))
+for j in range(9):
+	for i in range(m):
+		if exlist[i] == j:
+			one_v_all[i] = 1
+		else:
+			one_v_all[i] = 0
+exlist = "exlist" + str(j)
+
+
