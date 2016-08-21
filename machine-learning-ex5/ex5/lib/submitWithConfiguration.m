49812ea376e620c495e7df974993b9c4ba82186b
Julian Norton <Julian@JulianNorton.com>
2016-08-20 22:59:26 -0400

    5th assignment mac octave 4.0 fix

--- machine-learning-ex5/ex5/lib/submitWithConfiguration.m
+++ machine-learning-ex5/ex5/lib/submitWithConfiguration.m
@@ -65,7 +65,7 @@ function response = submitParts(conf, email, token, parts)
   submissionUrl = submissionUrl();
   params = {'jsonBody', body};
   %responseBody = urlread(submissionUrl, 'post', params);
-  [code, responseBody] = system(sprintf('echo jsonBody=%s | curl -k -X POST -d @- %s', body, submissionUrl));
+  [code, responseBody] = system(sprintf('echo ''jsonBody=%s'' | curl -k -X POST -d @- %s', body, submissionUrl));
   response = loadjson(responseBody);
 end
 
